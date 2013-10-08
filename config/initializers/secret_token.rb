7ad002ee71e9fb36d6b1c34e75f3a6860b0f15fd
alexandrdera <alexandr.dera@gmail.com>
2013-10-08 17:55:33 +0300

    before adding anothre model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '51fc12e043747fb067f1eca84ad5cef40cf2d5bd5eee550d37dd9f1d0ec2f67b36370303802cce9104e3feb060267fc85607a7c2994d2ac39995fdd72742a6c4'
